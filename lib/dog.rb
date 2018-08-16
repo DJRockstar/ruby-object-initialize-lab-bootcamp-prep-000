04dec80aca19437e10f73d12b8caa4cf1b83912e
DJ Jaiswal <digvijays18@gmail.com>
2018-08-16 22:04:11 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,5 @@
 class Dog
-  def initialize(name, breed="Mutt")
+  def initialize(name, breed)
     @name = name
     @breed = breed
   end
@@ -16,7 +16,7 @@ class Dog
   end
 
   def breed
-    @breeed = "Mia"
+    @breeed = "Huskey"
   end
 end
 
