82b700185aa5a450e0b4b01c041a5a39af18d565
DJ Jaiswal <digvijays18@gmail.com>
2018-08-16 21:57:21 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,20 +2,4 @@ class Person
   def initialize(name)
     @name = name
   end
-
-  def name=(dog_name)
-    @name = dog_name
-  end
-
-  def breed=(dog_breed)
-    @breed = dog_breed
-  end
-
-  def name
-    @name
-  end
-
-  def breed
-    @breeed
-  end
 end
