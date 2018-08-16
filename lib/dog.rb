c6bfc7b4a0a375168af4c980ec4f2b5713adc92b
DJ Jaiswal <digvijays18@gmail.com>
2018-08-16 21:57:10 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,5 +3,19 @@ class Dog
     @name = name
     @breed = "Mutt"
   end
-  
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+
+  def name
+    @name
+  end
+
+  def breed
+    @breeed
+  end
 end
