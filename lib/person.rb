ac2a9e7d61c7b472a82d3cefd09c91cb6e6d4a91
DJ Jaiswal <digvijays18@gmail.com>
2018-08-16 21:55:43 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,7 +3,19 @@ class Person
     @name = name
   end
 
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+
   def name
-    @name 
+    @name
+  end
+
+  def breed
+    @breeed
   end
 end
